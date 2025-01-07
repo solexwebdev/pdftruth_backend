9883305fef1d8293380ff58106519a08270bf637
solexwebdev <solex.webdev@gmail.com>
2025-01-07 18:25:58 +0200

    Remove unused seeder file

--- src/db/seeders/database.seeder.ts
+++
@@ -1,9 +0,0 @@
-import { EntityManager } from '@mikro-orm/postgresql';
-import { Seeder } from '@mikro-orm/seeder';
-import { UserSeeder } from '@/db/seeders/user.seeder';
-
-export class DatabaseSeeder extends Seeder {
-  async run(em: EntityManager): Promise<void> {
-    return this.call(em, [UserSeeder]);
-  }
-}

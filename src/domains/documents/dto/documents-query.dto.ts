c45523ab68dfee3b4381b968ca68bf96266edf3a
solexwebdev <solex.webdev@gmail.com>
2025-02-12 09:13:42 +0200

    Feat/15 fetch document list with filters
    - Updated query structure for fetching documents
    - Added field names for sorting
    - Added functionality to retrieve enum key by value

--- src/domains/documents/dto/documents-query.dto.ts
+++ src/domains/documents/dto/documents-query.dto.ts
@@ -1,8 +1,14 @@
 import { PaginateQuery } from '@/common/dto/paginate.query.dto';
 import { ApiPropertyOptional } from '@nestjs/swagger';
-import { IsNotEmpty, IsOptional } from 'class-validator';
+import { IsEnum, IsNotEmpty, IsOptional } from 'class-validator';
+import { SortDocuments } from '@/domains/documents/enums/sort-documents.enum';
 
 export class DocumentsQueryDto extends PaginateQuery {
+  @ApiPropertyOptional({ enum: SortDocuments, required: false, nullable: false })
+  @IsEnum(SortDocuments)
+  @IsOptional()
+  sortBy?: SortDocuments;
+
   @ApiPropertyOptional({ description: 'Search query' })
   @IsOptional()
   @IsNotEmpty()
---
+++ src/domains/documents/enums/sort-documents.enum.ts
@@ -0,0 +1,4 @@
+export enum SortDocuments {
+  'documents.createdAt' = 'documentDate',
+  'documents.name' = 'name',
+}
